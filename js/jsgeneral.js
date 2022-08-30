02e4832fb3c46a5187806f0064a46600e765c38e
Maarcaa <103992561+Maarcaa@users.noreply.github.com>
2022-08-30 16:41:14 +0200

    TrainingOver
    Fin de l'entraînement

---
+++ js/jsgeneral.js
@@ -0,0 +1,16 @@
+"use strict"
+
+
+
+
+
+
+// //ajoute du contenu dans createPara, du html
+// createPara.innerHTML = ` <span class="firstCaracPara">A</span>ssumenda provident quis enim
+// dolores? Est explicabo doloribus nobis illo sapiente laudantium
+// architecto earum odio molestias facilis aliquam optio obcaecati facere
+// minus excepturi repudiandae nesciunt placeat dolorem quasi error,
+// eveniet reprehenderit. Repellat, eius id delectus sapiente ratione
+// cumque earum deleniti totam quam?";`;
+
+// selectGameNotice.appendChild(createPara);
